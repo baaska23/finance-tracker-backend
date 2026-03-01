45ed502d041aa752ab61367d5358c04fceb89241
kaitheengineer <93702715+baaska23@users.noreply.github.com>
2026-03-01 17:07:16 +0800

    feat: restructure transaction module and add budget module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TransactionModule } from './transactions/transaction.module';
+import { TransactionModule } from './transaction/transaction.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
